4be779f19752e076348bc1b39e7a9c62faf1f5e9
Ad-rian27 <rian270627@gmail.com>
2026-07-09 21:50:16 +0530

    Completed student-app

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,29 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import AddStudent from './components/AddStudent'
+import SearchStudent from './components/SearchStudent'
+import DeleteStudent from './components/DeleteStudent'
+import ViewStudents from './components/ViewStudents'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <BrowserRouter>
+
+        <Routes>
+
+          <Route path='/' element={<AddStudent />} />
+          <Route path='/search' element={<SearchStudent />} />
+          <Route path='/delete' element={<DeleteStudent />} />
+          <Route path='/view' element={<ViewStudents />} />
+
+        </Routes>
+
+      </BrowserRouter>
     </>
   )
 }
