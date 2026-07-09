f6129d5d5850c814b1b70886cbc8d92a1c44a516
Ad-rian27 <rian270627@gmail.com>
2026-07-09 21:39:52 +0530

    Added ViewStudent

---
+++ src/components/ViewStudents.jsx
@@ -0,0 +1,90 @@
+import React from 'react'
+
+const ViewStudents = () => {
+    return (
+        <div>
+
+            <NavigationBar />
+            <div className="container">
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+                        <div className="row g-3">
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
+
+                                <div class="card">
+                                    <img src="https://randomuser.me/api/portraits/men/1.jpg" height="300px" class="card-img-top" alt="..." />
+                                    <div class="card-body">
+                                        <h6 class="card-title">Admission No: ADM1001</h6>
+                                        <h6 class="card-title">Name: Aarav Sharma</h6>
+                                        <h6 class="card-title">Class: 10</h6>
+                                        <a href="#" class="btn btn-success">Select</a>
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
+
+                                <div class="card">
+                                    <img src="https://randomuser.me/api/portraits/women/2.jpg" height="300px" class="card-img-top" alt="..." />
+                                    <div class="card-body">
+                                        <h6 class="card-title">Admission No: ADM1002</h6>
+                                        <h6 class="card-title">Name: Diya Nair</h6>
+                                        <h6 class="card-title">Class: 9</h6>
+                                        <a href="#" class="btn btn-success">Select</a>
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
+
+
+                                <div class="card">
+                                    <img src="https://randomuser.me/api/portraits/men/3.jpg" height="300px" class="card-img-top" alt="..." />
+                                    <div class="card-body">
+                                        <h6 class="card-title">Admission No: ADM1003</h6>
+                                        <h6 class="card-title">Name: Arjun Menon</h6>
+                                        <h6 class="card-title">Class: 8</h6>
+                                        <a href="#" class="btn btn-success">Select</a>
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
+
+                                <div class="card">
+                                    <img src="https://randomuser.me/api/portraits/women/4.jpg" height="300px" class="card-img-top" alt="..." />
+                                    <div class="card-body">
+                                        <h6 class="card-title">Admission No: ADM1004</h6>
+                                        <h6 class="card-title">Name: Ananya Joseph</h6>
+                                        <h6 class="card-title">Class: 10</h6>
+                                        <a href="#" class="btn btn-success">Select</a>
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
+
+                                <div class="card">
+                                    <img src="https://randomuser.me/api/portraits/men/5.jpg" height="300px" class="card-img-top" alt="..." />
+                                    <div class="card-body">
+                                        <h6 class="card-title">Admission No: ADM1005</h6>
+                                        <h6 class="card-title">Name: Rohan Patel</h6>
+                                        <h6 class="card-title">Class: 7</h6>
+                                        <a href="#" class="btn btn-success">Select</a>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+    )
+}
+
+export default ViewStudents
