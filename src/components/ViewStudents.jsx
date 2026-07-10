17b20a0c27907d77a3196fba7e88b628e0e7c12f
Ad-rian27 <rian270627@gmail.com>
2026-07-10 10:17:44 +0530

    Data dynamically added

--- src/components/ViewStudents.jsx
+++ src/components/ViewStudents.jsx
@@ -1,155 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NavigationBar from './NavigationBar'
 
 const ViewStudents = () => {
+
+    const [data, changeData] = useState([
+        { name: "Aarav Sharma", admn: "ADM1001", class: 10, avatar: "https://randomuser.me/api/portraits/men/1.jpg" },
+        { name: "Diya Nair", admn: "ADM1002", class: 11, avatar: "https://randomuser.me/api/portraits/women/2.jpg" },
+        { name: "Arjun Menon", admn: "ADM1003", class: 9, avatar: "https://randomuser.me/api/portraits/men/3.jpg" },
+        { name: "Ananya Joseph", admn: "ADM1004", class: 12, avatar: "https://randomuser.me/api/portraits/women/4.jpg" },
+        { name: "Rohan Patel", admn: "ADM1005", class: 7, avatar: "https://randomuser.me/api/portraits/men/5.jpg" }
+    ]);
+
     return (
         <div>
-
             <NavigationBar />
-            <div className="container">
-                <div className="row">
-                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
 
-                        <div className="row g-3">
-                            <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
+            <div className="container mt-4">
+                <div className="row g-3">
 
-                                <div class="card">
-                                    <img src="https://randomuser.me/api/portraits/men/1.jpg" height="300px" class="card-img-top" alt="..." />
-                                    <div class="card-body">
-                                        <h6 class="card-title">Admission No: ADM1001</h6>
-                                        <h6 class="card-title">Name: Aarav Sharma</h6>
-                                        <h6 class="card-title">Class: 10</h6>
-                                        <a href="#" class="btn btn-success">Select</a>
-                                    </div>
+                    {data.map((value, index) => (
+                        <div
+                            key={value.admn}
+                            className="col-12 col-sm-6 col-md-6 col-lg-4"
+                        >
+                            <div className="card h-100 shadow">
+
+                                <img
+                                    src={value.avatar}
+                                    height="300px"
+                                    className="card-img-top"
+                                    alt={value.name}
+                                />
+
+                                <div className="card-body">
+                                    <h6 className="card-title">
+                                        Admission No: {value.admn}
+                                    </h6>
+
+                                    <h6 className="card-title">
+                                        Name: {value.name}
+                                    </h6>
+
+                                    <h6 className="card-title">
+                                        Class: {value.class}
+                                    </h6>
+
+                                    <a href="#" className="btn btn-success">
+                                        Select
+                                    </a>
                                 </div>
 
                             </div>
-                            <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
-
-                                <div class="card">
-                                    <img src="https://randomuser.me/api/portraits/women/2.jpg" height="300px" class="card-img-top" alt="..." />
-                                    <div class="card-body">
-                                        <h6 class="card-title">Admission No: ADM1002</h6>
-                                        <h6 class="card-title">Name: Diya Nair</h6>
-                                        <h6 class="card-title">Class: 9</h6>
-                                        <a href="#" class="btn btn-success">Select</a>
-                                    </div>
-                                </div>
-
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
-
-
-                                <div class="card">
-                                    <img src="https://randomuser.me/api/portraits/men/3.jpg" height="300px" class="card-img-top" alt="..." />
-                                    <div class="card-body">
-                                        <h6 class="card-title">Admission No: ADM1003</h6>
-                                        <h6 class="card-title">Name: Arjun Menon</h6>
-                                        <h6 class="card-title">Class: 8</h6>
-                                        <a href="#" class="btn btn-success">Select</a>
-                                    </div>
-                                </div>
-
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
-
-                                <div class="card">
-                                    <img src="https://randomuser.me/api/portraits/women/4.jpg" height="300px" class="card-img-top" alt="..." />
-                                    <div class="card-body">
-                                        <h6 class="card-title">Admission No: ADM1004</h6>
-                                        <h6 class="card-title">Name: Ananya Joseph</h6>
-                                        <h6 class="card-title">Class: 10</h6>
-                                        <a href="#" class="btn btn-success">Select</a>
-                                    </div>
-                                </div>
-
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
-
-                                <div class="card">
-                                    <img src="https://randomuser.me/api/portraits/men/5.jpg" height="300px" class="card-img-top" alt="..." />
-                                    <div class="card-body">
-                                        <h6 class="card-title">Admission No: ADM1005</h6>
-                                        <h6 class="card-title">Name: Rohan Patel</h6>
-                                        <h6 class="card-title">Class: 7</h6>
-                                        <a href="#" class="btn btn-success">Select</a>
-                                    </div>
-                                </div>
-
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
-
-                                <div class="card">
-                                    <img src="https://randomuser.me/api/portraits/men/1.jpg" height="300px" class="card-img-top" alt="..." />
-                                    <div class="card-body">
-                                        <h6 class="card-title">Admission No: ADM1001</h6>
-                                        <h6 class="card-title">Name: Aarav Sharma</h6>
-                                        <h6 class="card-title">Class: 10</h6>
-                                        <a href="#" class="btn btn-success">Select</a>
-                                    </div>
-                                </div>
-
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
-
-                                <div class="card">
-                                    <img src="https://randomuser.me/api/portraits/men/1.jpg" height="300px" class="card-img-top" alt="..." />
-                                    <div class="card-body">
-                                        <h6 class="card-title">Admission No: ADM1001</h6>
-                                        <h6 class="card-title">Name: Aarav Sharma</h6>
-                                        <h6 class="card-title">Class: 10</h6>
-                                        <a href="#" class="btn btn-success">Select</a>
-                                    </div>
-                                </div>
-
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
-
-                                <div class="card">
-                                    <img src="https://randomuser.me/api/portraits/men/1.jpg" height="300px" class="card-img-top" alt="..." />
-                                    <div class="card-body">
-                                        <h6 class="card-title">Admission No: ADM1001</h6>
-                                        <h6 class="card-title">Name: Aarav Sharma</h6>
-                                        <h6 class="card-title">Class: 10</h6>
-                                        <a href="#" class="btn btn-success">Select</a>
-                                    </div>
-                                </div>
-
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
-
-                                <div class="card">
-                                    <img src="https://randomuser.me/api/portraits/men/1.jpg" height="300px" class="card-img-top" alt="..." />
-                                    <div class="card-body">
-                                        <h6 class="card-title">Admission No: ADM1001</h6>
-                                        <h6 class="card-title">Name: Aarav Sharma</h6>
-                                        <h6 class="card-title">Class: 10</h6>
-                                        <a href="#" class="btn btn-success">Select</a>
-                                    </div>
-                                </div>
-
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
-
-                                <div class="card">
-                                    <img src="https://randomuser.me/api/portraits/men/1.jpg" height="300px" class="card-img-top" alt="..." />
-                                    <div class="card-body">
-                                        <h6 class="card-title">Admission No: ADM1001</h6>
-                                        <h6 class="card-title">Name: Aarav Sharma</h6>
-                                        <h6 class="card-title">Class: 10</h6>
-                                        <a href="#" class="btn btn-success">Select</a>
-                                    </div>
-                                </div>
-
-                            </div>
-                            
                         </div>
+                    ))}
 
-                    </div>
                 </div>
             </div>
-
-
         </div>
     )
 }
